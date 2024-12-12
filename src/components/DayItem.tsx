74c469a7f481b2090821ab005b094d8a2419116e
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-12-12 12:25:43 -0300

    better star visuals

--- src/components/DayItem.tsx
+++ src/components/DayItem.tsx
@@ -1,11 +1,13 @@
 import dayjs from 'dayjs';
 import { IonItem, IonGrid, IonRow, IonCol, IonLabel, IonIcon } from '@ionic/react';
-import { starOutline } from 'ionicons/icons';
+import { starOutline, star, starSharp } from 'ionicons/icons';
 import { DayDetails } from '../types';
 
 import "./DayItem.css";
 
 export default function DayItem({ day: { date, activities } }: { day: DayDetails }) {
+
+  const pending = getPendingQ();
 
   function getPrimary() {
     return activities.find(a => a.primary);
@@ -33,14 +35,23 @@ export default function DayItem({ day: { date, activities } }: { day: DayDetails
             <strong>{date.format("DD")}</strong>
             <small>{date.format("MMM")}</small>
           </IonCol>
-          <IonCol className='duties' size='9'>
+          <IonCol className='duties' size='8'>
             <IonLabel>
               <h1 className='ellipsis'>{getPrimary()?.title}</h1>
-              <h2>{getPendingQ()} tareas pendientes - {getPendingTime()}</h2>
+              <h2>{pending} tareas pendientes - {getPendingTime()}</h2>
             </IonLabel>
           </IonCol>
-          <IonCol size='1'>
-            <IonIcon aria-hidden="true" icon={starOutline} />
+          <IonCol size='2' className='star'>
+            <div>
+              <IonIcon aria-hidden="true" icon={starOutline} />
+              {pending === 0 && <IonIcon aria-hidden="true" icon={starSharp} className="star-fill" />}
+            </div>
+
+            {/* <div className="icon-inner">
+              <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512">
+                <path d="M480 208H308L256 48l-52 160H32l140 96-54 160 138-100 138 100-54-160z" stroke-linejoin="round" className="ionicon-fill-none ionicon-stroke-width"></path>
+              </svg>
+            </div> */}
           </IonCol>
         </IonRow>
       </IonGrid>
